54c7d5f560dba184ff6c19a1e27dcc581ea64c9c
blatantBabbage <ransinghbel@gmail.com>
2022-01-23 20:46:01 +0530

    adding service layer

--- src/com/nitesh/springdemo/controller/CustomerController.java
+++ src/com/nitesh/springdemo/controller/CustomerController.java
@@ -2,9 +2,11 @@ package com.nitesh.springdemo.controller;
 
 import com.nitesh.springdemo.dao.CustomerDAO;
 import com.nitesh.springdemo.entity.Customer;
+import com.nitesh.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,13 +17,13 @@ public class CustomerController {
 
     // need to inject CustomerDAO dependency
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel) {
 
         // get the customers from the DAO
-        List<Customer> theCustomers = customerDAO.getCustomers();
+        List<Customer> theCustomers = customerService.getCustomers();
 
         // add the customer to the Model
         theModel.addAttribute("customers", theCustomers);
---
+++ src/com/nitesh/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.nitesh.springdemo.service;
+
+import com.nitesh.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+}
